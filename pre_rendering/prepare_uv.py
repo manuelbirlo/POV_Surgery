1288919e88740d82f8f6a1b095eb96d099ef2e18
Manuel Birlo <manuel.birlo.18@ucl.ac.uk>
2024-01-12 18:09:57 +0000

    Intermediate debug changes for some python scripts

--- pre_rendering/prepare_uv.py
+++ pre_rendering/prepare_uv.py
@@ -1,31 +1,50 @@
+"""
+Prepare UV map for body model. This script needs a 00000.ply full body model. It can be converted from the 00000.obj in MeshLab and copied into 
+/root/POV_Surgery/assets/transfer_surgical_Source/smplx_fitted_ply via docker cp.
+Output is an updated body .obj file (example: 00000.obj) that is saved to .../transfer_surgical_Source/texture_rotate/default_smplx_male.obj
+"""
+
 import open3d as o3d
 import numpy as np
 import os
 import shutil
 from tqdm import tqdm
 
-ROOT_DIR = '/home/ray/Downloads/zju-ls-feng/output/smplx'
+#ROOT_DIR = '/home/ray/Downloads/zju-ls-feng/output/smplx'
+ROOT_DIR  = '/root/POV_Surgery/assets/transfer_surgical_Source'
 OUT_dir = os.path.join(ROOT_DIR, 'texture_rotate')
 TMP_dir = os.path.join(ROOT_DIR, 'tmp')
 os.makedirs(TMP_dir,exist_ok=True)
 os.makedirs(OUT_dir,exist_ok=True)
-mesh = o3d.io.read_triangle_mesh('/home/ray/code_release/hand_texture/transfer_surgical_Source/test1am.obj',enable_post_processing=True)
+#mesh = o3d.io.read_triangle_mesh('/home/ray/code_release/hand_texture/transfer_surgical_Source/test1am.obj',enable_post_processing=True)
+mesh = o3d.io.read_triangle_mesh('/root/POV_Surgery/assets/transfer_surgical_Source/test1am.obj',enable_post_processing=True)
+
 mesh_save = mesh
+
+# This is the folder that contains the rotated body model .ply that was generated with 'python transfer_pose.py'.
 BASE_mesh = os.path.join(ROOT_DIR, 'rotated_body_ply')
 
+tqdm.write("________________________________________________________________________________________________________________")
 
 all_file_list = []
-for i in (range(0, 10000)):
-    this_mesh = os.path.join(BASE_mesh,str(i).zfill(5)+'.ply')
+#for i in (range(0, 10000)):
+for i in (range(0, 1)): # I use only 1 body .ply file (00000.obj) at the moment.
+    
+    this_mesh = os.path.join(BASE_mesh,str(i).zfill(5)+'.ply') # This has to be a body mesh, for example 00000.obj.
+    
     if not os.path.exists(this_mesh):
-        continue
+         print("continued!  {}".format(this_mesh))
+         continue
     all_file_list.append(this_mesh)
+    tqdm.write("_____________ appended {}".format(this_mesh))
 
 
 for i in tqdm(range(0, len(all_file_list)+1)):
     this_mesh = os.path.join(BASE_mesh,str(i).zfill(5)+'.ply')
     if not os.path.exists(this_mesh):
         continue
+
+    # Reads in a body .ply file but updates the respective body .obj file with vertices and triangles. Why is a .ply file being used here and not an .obj file as well?
     temp = o3d.io.read_triangle_mesh(this_mesh)
     # mesh.triangle_uvs
     mesh = mesh_save
@@ -34,8 +53,11 @@ for i in tqdm(range(0, len(all_file_list)+1)):
     # os.rename(os.path.join(OUT_dir, 'default_smplx_male.obj'), os.path.join(OUT_dir, str(i).zfill(5) + '.obj'))
     # o3d.io.write_triangle_mesh(os.path.join(OUT_dir,'default_smplx_male.obj'),mesh)
     o3d.io.write_triangle_mesh(os.path.join(TMP_dir, 'default_smplx_male.obj'), mesh)
+    tqdm.write("____ write____ {}".format(os.path.join(TMP_dir, 'default_smplx_male.obj')))
+
     shutil.move(os.path.join(TMP_dir, 'default_smplx_male.obj'),os.path.join(OUT_dir, str(i).zfill(5) + '.obj'))
     if i == 0 :
+        tqdm.write("__________i = 0")
         shutil.move(os.path.join(TMP_dir, 'default_smplx_male.mtl'), os.path.join(OUT_dir, 'default_smplx_male.mtl'))
         shutil.move(os.path.join(TMP_dir, 'default_smplx_male_0.png'), os.path.join(OUT_dir, 'default_smplx_male_0.png'))
 
--- pre_rendering/transfer_pose.py
+++ pre_rendering/transfer_pose.py
@@ -8,20 +8,33 @@ from slerp_utils import quaternion_from_matrix, quaternion_slerp, quaternion_mat
 
 
 ######################################3
-base_dir = '/home/ray/Downloads/zju-ls-feng/output/smplx'
+#base_dir = '/home/ray/Downloads/zju-ls-feng/output/smplx'
+# Assumption: The body model from /SMPLX_texture/SMPLX_texture/transfer_surgical_Source has been copied into /root/POV_Surgery/assets/transfer_surgical_Source.
+# The model model can be downloaded from https://drive.google.com/drive/folders/1nSDig2cEHscCPgG10-VcSW3Q1zKge4tP (SMPLX_texture.zip)
+base_dir = '/root/POV_Surgery/assets/transfer_surgical_Source'
 ######################################3
+
+# The following two directories should exist upfront and contain objct .ply files and a whole body .ply file. 
 rotated_body_ply = os.path.join(base_dir,'rotated_body_ply')
 rotated_object_ply = os.path.join(base_dir,'rotated_object_ply')
 os.makedirs(rotated_object_ply,exist_ok=True)
 os.makedirs(rotated_body_ply,exist_ok=True)
+
+# The following two directories are being created in this script and don't need to exist upfront.
 object_to_be_transfered = os.path.join(base_dir,'smplx_fitted_ply_Object')
 body_to_be_transfered = os.path.join(base_dir, 'smplx_fitted_ply')
-list_of_ply = os.listdir(body_to_be_transfered)
+os.makedirs(object_to_be_transfered,exist_ok=True)
+os.makedirs(body_to_be_transfered,exist_ok=True)
+
+#list_of_ply = os.listdir(body_to_be_transfered)
+list_of_ply = os.listdir(object_to_be_transfered)
+
 for i in tqdm(range(len(list_of_ply))):
 
     mesh_body = o3d.io.read_triangle_mesh(os.path.join(body_to_be_transfered, list_of_ply[i]))
     rr = R.from_euler('xyz',[90,0,0],degrees=True).as_matrix()
     mesh_body.rotate(rr, center=(0, 0, 0))
+    print("___ WRITING body : {}".format(os.path.join(rotated_body_ply,list_of_ply[i])))
     o3d.io.write_triangle_mesh(os.path.join(rotated_body_ply,list_of_ply[i]), mesh_body)
 
 
@@ -30,4 +43,5 @@ for i in tqdm(range(len(list_of_ply))):
     mesh_object.rotate(rr, center=(0, 0, 0))
 
     o3d.io.write_triangle_mesh(os.path.join(rotated_object_ply,list_of_ply[i]), mesh_object)
+    print("___ WRITING object: {}".format(os.path.join(rotated_object_ply,list_of_ply[i])))
 
